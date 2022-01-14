a0b1648941a1971f0ed85543e1a5172b08aafc37
Leo Omaña <leotangram@gmail.com>
2022-01-13 20:58:07 -0500

    JS fundamentals added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import "./App.css";
+import './App.css'
+import jsFundamentals from './js-fundamentals'
+jsFundamentals()
 
 function App() {
   return (
     <div className="App">
       <h1>Hola, inicio de nuestro e-commerce</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
