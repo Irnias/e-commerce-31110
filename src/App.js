41ce4a5e6469a0373667da73d0471d5e6a3d8449
Leo Omaña <leotangram@gmail.com>
2022-02-07 21:38:24 -0500

    Class-09 Events

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import ResizeComponent from "./components/resize-component/ResizeComponent";
 import Routes from "./routes/Routes";
 
 function App() {
   return (
     <div className="App">
+      <ResizeComponent />
       <Routes />
     </div>
   );
